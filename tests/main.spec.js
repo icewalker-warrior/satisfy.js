9e93657a2182d4d325ac80de8fe78e41b283925b
miguel <miguel@MacBook-Air-de-miguel.local>
2019-07-10 18:34:39 -0300

    first commit

---
+++ tests/main.spec.js
@@ -0,0 +1,9 @@
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+global.fetch = require('node-fetch');
+
