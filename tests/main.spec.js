67bf01e9ee432d618158b9dda2e027b61f3d173c
miguel <miguel@MacBook-Air-de-miguel.local>
2019-07-10 22:20:35 -0300

    smoke test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,8 +2,25 @@ import chai, {expect} from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
+import {input} from '../src/main';
 
 chai.use(sinonChai);
 sinonStubPromise(sinon);
 global.fetch = require('node-fetch');
+
+describe('Main', () => {
+
+  context('Smoke Tests', () => {
+    
+    it('Should be exist an function input()', () => {
+      expect(input).to.exist;
+    });
+
+    it('Should be an function', () => {
+      expect(input).to.be.an('function');
+    });
+
+  });
+
+});
 
